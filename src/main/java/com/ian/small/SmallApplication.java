2fff0526d620c700aa56b7e992dfd2ac324e939f
Ian Zheng <flks_2005@qq.com>
2017-11-30 13:47:40 +0800

    setup

---
+++ src/main/java/com/ian/small/SmallApplication.java
@@ -0,0 +1,12 @@
+package com.ian.small;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallApplication.class, args);
+	}
+}
