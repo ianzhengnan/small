2e53e42364d9fa03d6a1e7f357aee4324101c2f2
Ian Zheng <flks_2005@qq.com>
2017-12-06 17:52:39 +0800

    add Account domain2

--- src/main/java/com/ian/small/domain/Account.java
+++ src/main/java/com/ian/small/domain/Account.java
@@ -3,14 +3,58 @@ package com.ian.small.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 @Entity
-public class Account {
+public class Account implements Serializable{
 
     @Id
     @GeneratedValue
     private Long id;
-    private Long user_id;
-    private Long acc_id;
-    private String acc_type;
+    @NotNull
+    private Long userId;
+    private Long accId;
+    private String accType;
+
+    public Account() {
+    }
+
+    public Account(Long userId, Long accId, String accType) {
+        this.userId = userId;
+        this.accId = accId;
+        this.accType = accType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getAccId() {
+        return accId;
+    }
+
+    public void setAccId(Long accId) {
+        this.accId = accId;
+    }
+
+    public String getAccType() {
+        return accType;
+    }
+
+    public void setAccType(String accType) {
+        this.accType = accType;
+    }
 }
--- src/main/java/com/ian/small/domain/User.java
+++ src/main/java/com/ian/small/domain/User.java
@@ -2,9 +2,12 @@ package com.ian.small.domain;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 @Entity
-public class User {
+public class User implements Serializable{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,8 +19,100 @@ public class User {
     @NotNull
     private String alias;
     private String tel;
+    private Date birthday;
+    private String avatarUrl;
+    private Date createAt;
+    private Date lastLogonAt;
+    @OneToMany
+    private List<Account> accounts;
 
+    public User() {
 
+    }
 
+    public User(String username, String password, String alias) {
+        this.username = username;
+        this.password = password;
+        this.alias = alias;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getLastLogonAt() {
+        return lastLogonAt;
+    }
+
+    public void setLastLogonAt(Date lastLogonAt) {
+        this.lastLogonAt = lastLogonAt;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
 }
